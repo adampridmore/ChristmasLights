5c464d5c5dc17d0f509067f1b18ca0884e7f7f56
Adam Pridmore <adam_pridmore@hotmail.com>
2018-11-10 21:30:47 +0000

    - Parses xmas light commangs (in Script1.fsx)

--- solutions/AdamGrantFsharpChristmasLights/ChristmasLights/Script1.fsx
+++ solutions/AdamGrantFsharpChristmasLights/ChristmasLights/Script1.fsx
@@ -1,7 +1,4 @@
-﻿//#r @"C:\Users\adam_\.nuget\packages\fparsec\1.0.3\lib\netstandard1.6\FParsecCS.dll"
-//#r @"C:\Users\adam_\.nuget\packages\fparsec\1.0.3\lib\netstandard1.6\FParsec.dll"
-
-#r @"C:\Users\adam_\.nuget\packages\fparsec\1.0.3\lib\net40-client\FParsecCS.dll"
+﻿#r @"C:\Users\adam_\.nuget\packages\fparsec\1.0.3\lib\net40-client\FParsecCS.dll"
 #r @"C:\Users\adam_\.nuget\packages\fparsec\1.0.3\lib\net40-client\FParsec.dll"
 
 open FParsec
@@ -11,18 +8,36 @@ let test p str =
     | Success(result, _, _)   -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
+type Coord = int*int
+type Square = Coord*Coord
 type Cmd = 
-    | Off of int*int
-    | On of int*int
+    | Off of Square
+    | On of Square
 
-//let point = pstring "(" >>. sepBy pint32 (pstring ",") .>> pstring ")"
-//let twoPoints = many point 
+let str s = pstring s
+let betweenStrings s1 s2 p = str s1 >>. p .>> str s2
+//let floatBetweenBrackets = pfloat |> betweenStrings "[" "]"
 
-let pcoord = tuple2 (pint32 .>> pchar ',') pint32
+let pcoord = tuple2 (pint32 .>> pchar ',') pint32 |>> Coord
+test pcoord "1,2"
 
-let poff = pstring "OFF " >>. pcoord  |>> Off
-let pon = pstring "ON " >>. pcoord |>> On
-let pcommand = pon <|> poff
+let pbracketcoord = betweenStrings "(" ")" pcoord
+test pbracketcoord "(1,2)"
+
+let psquare = pipe2 pbracketcoord pbracketcoord (fun a b -> Square(a,b))
+test psquare "(1,2)(3,4)"
+
+let poff = pstring "OFF " >>. psquare |>> Off
+test poff "OFF (1,2)(3,4)"
+
+let pon = pstring "ON " >>. psquare |>> On
+test pon "ON (1,2)(3,4)"
+
+let pcommand = spaces >>. (pon <|> poff)
+test pcommand " 
+ON (1,2)(3,4)"
+test pcommand " OFF (1,2)(3,4)"
+
 let pmanycommands = many pcommand
-
-test pmanycommands @"OFF 1,99ON 33,66"
+test pmanycommands @"OFF (1,2)(3,4)
+ON (5,6)(7,8)"
---
+++ solutions/AdamGrantFsharpChristmasLights/ChristmasLights/Script2.fsx
@@ -0,0 +1,22 @@
+﻿#r @"C:\Users\adam_\.nuget\packages\fparsec\1.0.3\lib\net40-client\FParsecCS.dll"
+#r @"C:\Users\adam_\.nuget\packages\fparsec\1.0.3\lib\net40-client\FParsec.dll"
+
+open FParsec
+
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+let str s = pstring s
+
+let betweenStrings s1 s2 p = str s1 >>. p .>> str s2
+
+let floatBetweenBrackets = pfloat |> betweenStrings "[" "]"
+
+let myFile = many floatBetweenBrackets
+
+test myFile @""
+test myFile @"[1.2]"
+test myFile @"[1.2][2.3]"
+
